68ba9a530dfa600b1eff9b835e4097acf4135d1f
Erik Kopcha <erickopcha@gmail.com>
2020-12-08 12:46:07 +0200

    upload

---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { moviesData } from "../moviesData";
+import MovieItem from "./MovieItem";
+
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: moviesData,
+      moviesWillWatch: []
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-9">
+            <div className="row">
+              {this.state.movies.map((movie) => {
+                return (
+                  <div className="col-6 mt-3">
+                    <MovieItem
+                      key={movie.id}
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                      self={this}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="col-3 mt-3">
+            <p>Will Watch: {this.state.moviesWillWatch.length}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  addMovieToWillWatch(movie) {
+    // const updateWillWatch = [...this.state.moviesWillWatch];
+    // updateWillWatch.push(movie);
+
+    const updateWillWatch = [...this.state.moviesWillWatch, movie];
+
+    this.setState({
+      moviesWillWatch: updateWillWatch,
+    });
+  }
+
+  removeMovie(movie) {
+    const updateMovies = this.state.movies.filter((item) => {
+      return item.id !== movie.id;
+    });
+
+    this.setState({
+      movies: updateMovies,
+    });
+  }
+}
