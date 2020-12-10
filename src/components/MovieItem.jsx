27b97dc63786c3b9bbf5ee8ad87c2bb1ba1f6663
Erik Kopcha <erickopcha@gmail.com>
2020-12-10 17:40:26 +0200

    Update MovieItem.jsx

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,35 +1,65 @@
 import React from "react";
 
-const MovieItem = (props) => {
-  const { movie, removeMovie, addMovieToWillWatch } = props;
+class MovieItem extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <div className="card">
+    this.state = {
+      willWatch: false
+    };
+  }
+
+  render() {
+    const { self, movie, removeMovieFromWillWatch, addMovieToWillWatch } = this.props;
+
+    return (
+      <div key={movie.id} className="card">
       <img 
         className="card-img-top"
         src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path || movie.poster_path}`} 
-        alt="card image"/>
-      <div className="card-body" key={movie.id}>
+        alt="card"/>
+      <div className="card-body">
         <h6 className="card-title">{movie.title}</h6>
         <div className="d-flex justify-content-between align-items-center">
           <p className="mb-0">Rating: {movie.vote_average}</p>
-          <button
-            type="button" 
-            className="btn btn-primary"
-            onClick={addMovieToWillWatch.bind(props.self, props.movie)}>
-            Will watch
-          </button>
+          {this.state.willWatch ?
+            <button
+              type="button" 
+              className="btn btn-warning"
+              onClick={() => {
+                this.setState({
+                  willWatch: false
+                });
+
+                removeMovieFromWillWatch(movie)
+              }}>
+              Remove Will watch
+            </button>
+          :
+            <button
+              type="button" 
+              className="btn btn-success"
+              onClick={() => {
+                this.setState({
+                  willWatch: true
+                });
+
+                addMovieToWillWatch(movie)
+              }}>
+              Add Will watch
+            </button>
+          }
         </div>
         <button
           type="button"
           className="btn btn-danger"
-          onClick={removeMovie.bind(props.self, props.movie)}>
+          onClick={removeMovieFromWillWatch.bind(self, movie)}>
           Delete movie
         </button>
       </div>
     </div>
-
-  );
-};
+    );
+  }
+}
 
 export default MovieItem;
