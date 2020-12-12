d8f48c96a2e19ff5b16013a0a9c62112b19f76e9
Erik Kopcha <erickopcha@gmail.com>
2020-12-12 18:47:19 +0200

    build

---
+++ build/precache-manifest.1640a30bef9678be28521333b7891198.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b3fe27492cd027fd33df5515b3ad3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7da377adce192b638622",
+    "url": "/static/css/main.38ccf473.chunk.css"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/static/js/2.6481e93c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6481e93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da377adce192b638622",
+    "url": "/static/js/main.9a48e9e7.chunk.js"
+  },
+  {
+    "revision": "f7f1314cc112a19105f1",
+    "url": "/static/js/runtime-main.608f926c.js"
+  }
+]);
