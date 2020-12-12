1f473a21a44c68e8182ac36cb9f616c800278707
Erik Kopcha <erickopcha@gmail.com>
2020-12-12 18:50:41 +0200

    Updates

---
+++ precache-manifest.bb5fe2352c7636ae02fa849e45b97fee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7729dabd246d3eb43670680d51ba96",
+    "url": "/react-w-movies/index.html"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/react-w-movies/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7da377adce192b638622",
+    "url": "/react-w-movies/static/css/main.38ccf473.chunk.css"
+  },
+  {
+    "revision": "aa50e8f3f370df29a45c",
+    "url": "/react-w-movies/static/js/2.6481e93c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-w-movies/static/js/2.6481e93c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da377adce192b638622",
+    "url": "/react-w-movies/static/js/main.9a48e9e7.chunk.js"
+  },
+  {
+    "revision": "6be68ec90dabcd74b8cb",
+    "url": "/react-w-movies/static/js/runtime-main.4bb554c5.js"
+  }
+]);
